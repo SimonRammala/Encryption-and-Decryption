f5635d68a9549e1986db99e1b502a2295afa0dc0
Simon Rammala <simonrammala456@gmail.com>
2024-01-18 15:26:16 +0200

    Add an if statment select a function from the encrypt and decrypt files

--- index.py
+++ index.py
@@ -4,3 +4,16 @@ from art import *
 greetings = "Welcome to the realm of text \n message encryption and decryption."
 tprint(greetings)
 
+print("==============================================================================================================================================================================================")
+
+
+
+
+option = input("Type 'encode' to encrypt or type 'decode' to decrypt =>").lower()
+
+if(option == 'encode'):
+    print("we")
+elif(option == 'decode'):
+    print('what')
+else:
+    print("You have entered an invalide option, Try again")
