29209af07f44cb84ed1e4076219774f246b5001b
Simon Rammala <simonrammala456@gmail.com>
2024-01-30 21:31:14 +0200

    Changed the name of the file from 'letters' to 'characters. 1. Added a new list that contains only spetical characters

---
+++ characters.py
@@ -0,0 +1,5 @@
+# List of uppercase letters representing the English alphabet
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+# List contains common special characters that are not numbers or letters
+special_characters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
