0adc26262b01c939b4ef7a30c7606a70e0271ed5
Simon Rammala <simonrammala456@gmail.com>
2024-01-30 21:20:09 +0200

    Added one line of cooment

--- letters.py
+++ letters.py
@@ -1,2 +1,2 @@
+# List of uppercase letters representing the English alphabet
 alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
